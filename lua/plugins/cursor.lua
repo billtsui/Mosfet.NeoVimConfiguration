fbc2a05197d9474f1901b2bf1fa4265709c0cb21
Bill.K.Tsui <bill.k.tsui@gmail.com>
2026-07-17 16:42:54 +0800

    :sparkles: 增加光标粒子特效

---
+++ lua/plugins/cursor.lua
@@ -0,0 +1,16 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    -- 启用粒子效果
+    particles_enabled = true,
+    particle_max_num = 200,
+    particle_spread = 0.5,
+    never_draw_over_target = true,
+    -- 粒子数量
+    particles_per_second = 200,
+    -- 尾迹回弹阻尼（数字越小越Q弹）
+    damping = 0.85,
+    -- 粒子扩散速度
+    particle_max_initial_velocity = 15,
+  },
+}
