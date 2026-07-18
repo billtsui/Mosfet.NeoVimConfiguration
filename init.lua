3288e8a6619eb255ebe83b488ad4d6130b5cf8fa
Bill.K.Tsui <bill.k.tsui@gmail.com>
2026-07-18 16:27:15 +0800

    :sparkles: 调整Explorer的布局

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.opt.smartindent = true -- 智能缩进（根据代码语法自动对齐）
+vim.opt.expandtab = true -- 将 Tab 键转换为缩进空格
+vim.opt.tabstop = 4 -- 1个 Tab 占 4 个空格
+vim.opt.shiftwidth = 4 -- 换行时自动缩进 4 个空格
 
 -- 自动同步 Neovim 背景色到 Kitty
 vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
