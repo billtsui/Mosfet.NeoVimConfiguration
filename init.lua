d65e89e3c4f0920613a89af1a2f75945d5275cd5
Bill.K.Tsui <bill.k.tsui@gmail.com>
2026-07-18 14:54:19 +0800

    :bug: Fix a bug.

--- init.lua
+++ init.lua
@@ -1,11 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
 
 -- 自动同步 Neovim 背景色到 Kitty
 vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
--- lua/plugins/cyberdream.lua
+++ lua/plugins/cyberdream.lua
@@ -5,11 +5,14 @@ return {
     priority = 1000,
     opts = {
       transparent = true,
-      overrides = function(colors)
+      overrides = function()
         return {
           Comment = { fg = "#008000", bg = "NONE", italic = true },
           LineNr = { fg = "#808080" },
-          CursorLineNr = { fg = colors.green, bold = true },
+          CursorLineNr = { fg = "#FF9E3B", bold = true },
+          SnacksPickerGitStatusAdded = { fg = "#E0E0E0", bold = true },
+          SnacksPickerGitStatusUntracked = { fg = "#B5E1E6", italic = true },
+          SnacksPickerGitStatusModified = { fg = "#FF9E3B", bold = true },
         }
       end,
     },
